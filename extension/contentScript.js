192aa9a09ead5ccc1286001dc27c61c3ee026fbe
Andy Valerio <andy@valerio.nu>
2024-05-25 21:00:35 +0200

    Remove unused options

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -5,27 +5,17 @@ const config = { attributes: false, childList: true, subtree: true };
 const callback = function(mutationsList, observer) {
     var mapSize = '200';
     var mapUrl = 'https://www.valerio.nu/maps';
-    var mapKey = '';
     chrome.storage.sync.get({
         ppLocationMapSize: 'default',
-        ppLocationMapUrl: 'https://www.valerio.nu/maps',
-        ppLocationMapKey: ''
     }, function(items) {
         mapSize = items.ppLocationMapSize;
-        if (typeof items.ppLocationMapUrl !== 'undefined' && items.ppLocationMapUrl !== '') {
-            mapUrl = items.ppLocationMapUrl;
-        }
-        if (typeof items.ppLocationMapKey !== 'undefined' && items.ppLocationMapKey !== '') {
-            mapKey = items.ppLocationMapKey;
-        }
         for(const mutation of mutationsList) {
             if (mutation.type === 'childList') {
                 if (mutation.addedNodes[0] !== undefined && mutation.addedNodes[0].className === 'p-tab p-tab-photo-details') {
                     setTimeout(function(){
                         var countryElement = document.getElementsByClassName('input-country')[0];
                         var iframe = document.createElement('iframe');
-                        var keyParameter = mapKey !== '' ? 'customKey=' + mapKey : '';
-                        iframe.src = mapUrl + '?' + keyParameter + '&initLatitude=' + 
+                        iframe.src = mapUrl + '?initLatitude=' + 
                         document.getElementsByClassName('input-latitude')[0].firstChild.firstChild.firstChild.childNodes[1].value
                         + '&initLongitude=' + document.getElementsByClassName('input-longitude')[0].firstChild.firstChild.firstChild.childNodes[1].value;
                         iframe.style = 'display: block; width: 100%; height: 100%; border: none; min-height: ' + mapSize + 'px;';
--- extension/options.js
+++ extension/options.js
@@ -1,13 +1,9 @@
 // Saves options to chrome.storage
 function save_options() {
     var size = document.getElementById('size').value;
-    var url = document.getElementById('url').value;
-    var key = document.getElementById('key').value;
 
     chrome.storage.sync.set({
       ppLocationMapSize: size,
-      ppLocationMapUrl: url,
-      ppLocationMapKey: key
     }, function() {
       // Update status to let user know options were saved.
       var status = document.getElementById('status');
@@ -24,12 +20,8 @@ function save_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
         ppLocationMapSize: 'default',
-        ppLocationMapUrl: 'https://www.valerio.nu/maps',
-        ppLocationMapKey: 'AIzaSyBHtMaeHXRipKOibjU6SjvGA2kkT-EskuQ'
     }, function(items) {
       document.getElementById('size').value = items.ppLocationMapSize;
-      document.getElementById('url').value = items.ppLocationMapUrl;
-      document.getElementById('key').value = items.ppLocationMapKey;
     });
   }
   document.addEventListener('DOMContentLoaded', restore_options);
